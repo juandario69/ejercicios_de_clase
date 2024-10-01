3a5e516d0f57a957e8a12f64e1f3451d4b41aba3
juandario69 <munoz.moron.juan.david@iescamas.es>
2024-10-01 11:08:31 +0200

    Add files via upload

---
+++ convertidor_euro_a_dolar.py
@@ -0,0 +1,7 @@
+# esto es un programa que convierte de euro a dolar
+print("dime una cantidad")
+num1 = int(input())
+num2 = float(1.10)
+# realizamos la operacion
+total = num1*num2
+print("el resultado de este cambio es ", num1, "+", num2, "=", total, "$")
---
+++ ejercicio.2.py
@@ -0,0 +1,6 @@
+# esto calcula el 15 por ciento de una propina
+print("¿cuanto ha costado la cuenta?")
+euro= int(input())
+tasa= float(0.15)
+total = euro * tasa
+print ("tu propina es ", total)
