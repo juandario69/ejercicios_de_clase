53356da8e34f605ae0cf312682f82872935c4283
juandavid <munoz.moron.juan.david@iescamas.es>
2024-10-07 10:04:28 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+#Este programa te dice tu índice de masa corporal
+print("¿cuánto pesas en kg?")
+peso = int(input())
+print("¿cuánto mides en metros?")
+altura = float(input())
+IMC = peso / (altura = altura )
+print("tu índice de masa corporal es" , IMC )
