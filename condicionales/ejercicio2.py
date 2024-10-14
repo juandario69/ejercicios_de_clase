be121ac149a81a35ffa2322486682cdcb7cef2d1
juandavid <munoz.moron.juan.david@iescamas.es>
2024-10-14 09:53:32 +0200

    Add files via upload

---
+++ condicionales/ejercicio2.py
@@ -0,0 +1,8 @@
+#ejercicio2
+NUMEROALEATORIO = 3
+print("dime un numero del 1 al 10")
+numero=input()
+if (numero == NUMEROALEATORIO):
+    print(" has acertado")
+else:
+    print(" intentalo otra vez")
