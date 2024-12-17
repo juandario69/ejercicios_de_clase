4dfc342b7712d13b529328e4804383230ecf5c7e
juandavid <munoz.moron.juan.david@iescamas.es>
2024-12-17 10:43:16 +0100

    Add files via upload

---
+++ nota.py
@@ -0,0 +1,10 @@
+def calificacion():
+    nota = int(input(5)
+    if (nota=>90):
+    print("sobresaliente")
+    elif (nota=>70)and(nota=<89):
+    print("notable")
+    elif (nota=>50)and(nota=<69):
+    print("aprobado")
+    elif (nota=<49):
+    print ("suspenso")
